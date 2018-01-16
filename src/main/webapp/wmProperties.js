7525621c2c2165399cc84a992ca84f79a6c210d1
Sushma Imaginea <sushma.maddirala@imaginea.com>
2018-01-16 06:48:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_july26_1.0_jan",
+  "homePage" : "Main",
+  "name" : "SalesVision_july26_1_0_jan",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
